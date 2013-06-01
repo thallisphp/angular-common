c0709d8937ca0be5afac10a66331f7e770cbc1c5
Michael Calkins <michael@clouddueling.com>
2013-06-01 09:55:25 -0700

    Update redactor.js

--- directives/redactor.js
+++ directives/redactor.js
@@ -1,17 +1,43 @@
-directivesModule.directive("redactor", function () {
-  var linkFn = function (scope, el, attr, ngModel) {
-		scope.redactor = el.redactor({
-			focus: false,
-			callback: function (o) {
-				el.keydown(function () {
-					scope.$apply(ngModel.$setViewValue(o.getCode()));
-				});
-			}
-		});
-	}
-	return {
-		require: '?ngModel',
-		link: linkFn,
-		restrict: 'A',
-	};
+/*
+// http://stackoverflow.com/a/16428909/1069899
+
+HTML View:
+<div ng-controller="PostCtrl">
+    <form ng-submit="addPost()">
+        <textarea ng-model="newPost.content" redactor required></textarea>
+        <br />
+        <input type="submit" value="add post">
+    </form>
+
+    {{newPost.content}} <!-- This outputs the raw html with tags -->
+    <br />
+    <div ng-bind-html-unsafe="newPost.content"></div> <!-- This outputs the html -->
+</div>
+
+Controller function:
+$scope.addPost = function() {
+  var post;
+  post = Post.save($scope.newPost);
+  console.log(post);
+  $scope.posts.unshift(post);
+  return $scope.newPost.content = "<p>Add a new post...</p>";
+};
+*/
+directives.directive("redactor", function() {
+  return {
+    require: "?ngModel",
+    link: function($scope, elem, attrs, controller) {
+      return controller.$render = function() {
+        elem.redactor({
+          keyupCallback: function() {
+            return $scope.$apply(controller.$setViewValue(elem.getCode()));
+          },
+          execCommandCallback: function() {
+            return $scope.$apply(controller.$setViewValue(elem.getCode()));
+          },
+        });
+        return elem.setCode(controller.$viewValue);
+      };
+    }
+  };
 });
