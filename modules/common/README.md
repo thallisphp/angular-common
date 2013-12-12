82d201dcc297da1d16dbd604d4d76c691d67b9eb
Michael Calkins <michael@clouddueling.com>
2013-12-11 16:11:13 -0800

    added the master common file

---
+++ modules/common/README.md
@@ -0,0 +1,3 @@
+This file simply includes all common modules if you need them all or you can modify it to include less or more.
+
+Then you just need to type `common` into your modules instead of listing them all.
---
+++ modules/common/common.js
@@ -0,0 +1,21 @@
+(function() {
+    
+    "use strict";
+    
+    angular.module('common', [
+        'common.api',
+        'common.confirm',
+        'common.dateRange',
+        'common.drag',
+        'common.mediaelement',
+        'common.modal',
+        'common.ngBindHtmlUnsafe',
+        'common.print',
+        'common.redactor',
+        'common.strings',
+        'common.time',
+        'common.upload',
+        'common.youtube'
+    ]);
+
+})();
