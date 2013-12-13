abc6695d838898d1ca2b22eb0214ab8fe03e7c6d
Michael Calkins <michael@clouddueling.com>
2013-12-13 09:48:35 -0800

    code update

--- modules/api/api.js
+++ modules/api/api.js
@@ -5,6 +5,15 @@
     angular.module('common.api', [])
     
     .factory('Api', ['$http', function($http) {
+        /**
+         * @api {private} encodeObject(obj); encodeObject()
+         * @apiName encodeObject
+         * @apiGroup API
+         *
+         * @apiParam {Object} obj The object to be converted into a http query string.
+         *
+         * @apiSuccess {String} str http query string.
+         */
         function encodeObject(obj) {
             var str = "";
             for(var p in obj) {
@@ -33,25 +42,84 @@
             return str;
         }
         
-        var base = '',
-            api = {};
+        var base = '';
+        var api = {};
         
+        
+        
+        /**
+         * @api {public} Api.setBase('/api/v2/'); setBase()
+         * @apiName setBase
+         * @apiGroup API
+         *
+         * @apiParam {String} newBase The new base url for all $http requests.
+         *
+         * @apiSuccess {Null} null
+         */
         api.setBase = function(newBase) {
             base = newBase;
         };
         
+        
+        
+        /**
+         * @api {public} Api.get(url,data); get()
+         * @apiName get
+         * @apiGroup API
+         *
+         * @apiParam {String} url Get destination.
+         * @apiParam {Object} data Data to be sent with request.
+         *
+         * @apiSuccess {Promise} promise Standard $http promise.
+         */
         api.get = function(url, data){
             return $http.get(base + url + "?" + encodeObject(data));
         };
         
+        
+        
+        /**
+         * @api {public} Api.get(url,data); get()
+         * @apiName get
+         * @apiGroup API
+         *
+         * @apiParam {String} url Get destination.
+         * @apiParam {Object} data Data to be sent with request.
+         *
+         * @apiSuccess {Promise} promise Standard $http promise.
+         */
         api.post = function(url, data){
             return $http.post(base + url, data);
         };
         
+        
+        
+        /**
+         * @api {public} Api.get(url,data); get()
+         * @apiName get
+         * @apiGroup API
+         *
+         * @apiParam {String} url Get destination.
+         * @apiParam {Object} data Data to be sent with request.
+         *
+         * @apiSuccess {Promise} promise Standard $http promise.
+         */
         api.put = function(url, data){
             return $http.put(base + url, data);
         };
         
+        
+        
+        /**
+         * @api {public} Api.get(url,data); get()
+         * @apiName get
+         * @apiGroup API
+         *
+         * @apiParam {String} url Get destination.
+         * @apiParam {Object} data Data to be sent with request.
+         *
+         * @apiSuccess {Promise} promise Standard $http promise.
+         */
         api.delete = function(url, data){
             return $http.delete(base + url + "?" + encodeObject(data));
         };
