feb5929f3417e132801b6eadd2a91bb3b00021ca
Michael Calkins <michael@clouddueling.com>
2013-12-15 22:53:32 -0800

    code update

--- app.js
+++ app.js
@@ -9,8 +9,21 @@
 
     .controller('MainCtrl', ['$scope', function($scope) {
         $scope.modules = [
+            'common',
+            'api',
+            'confirm',
+            'dateRange',
             'drag',
             'dragdrop',
+            'mediaelement',
+            'modal',
+            'ngBindHtmlUnsafe',
+            'print',
+            'redactor',
+            'strings',
+            'time',
+            'upload',
+            'youtube'
         ];
     }])
     
