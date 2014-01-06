4457c1f75fdb4c8ca956dd1a2e83a31859a5fb40
Michael Calkins <michael@clouddueling.com>
2014-01-05 16:18:34 -0800

    tests for the api service

--- modules/api/api.js
+++ modules/api/api.js
@@ -1,130 +1,123 @@
 (function() {
-    
+
     "use strict";
-    
+
     angular.module('common.api', [])
-    
-    .factory('Api', ['$http', function($http) {
-        /**
-         * @api {private} encodeObject(obj); encodeObject()
-         * @apiName encodeObject
-         * @apiGroup API
-         *
-         * @apiParam {Object} obj The object to be converted into a http query string.
-         *
-         * @apiSuccess {String} str http query string.
-         */
-        function encodeObject(obj) {
-            var str = "";
-            for(var p in obj) {
-                if (obj[p]) {
-                    switch(typeof obj[p]) {
-                    case 'boolean' :
-                        str += p + '=' + parseInt(obj[p], 10) + "&";
-                        break;
-    
-                    case 'number' :
-                    case 'string' :
-                        str += p + "=" + obj[p] + "&";
-                        break;
-    
-                    case 'object' :
-                        for (var i in obj[p]) {
-                            if (obj[p][i]) {
-                                str += p + "[" + encodeURIComponent(i) + "]=" + encodeURIComponent(obj[p][i]) + "&";
-                            }
+
+        .factory('Api', ['$http', function($http) {
+            this.base = '';
+
+            /**
+             * @api {private} encodeObject(obj); encodeObject()
+             * @apiName encodeObject
+             * @apiGroup API
+             *
+             * @apiParam {Object} obj The object to be converted into a http query string.
+             *
+             * @apiSuccess {String} str http query string.
+             */
+            this.encodeObject = function(obj) {
+                var str = "";
+                for(var p in obj) {
+                    if (obj[p]) {
+                        switch(typeof obj[p]) {
+                            case 'boolean' :
+                                str += p + '=' + parseInt(obj[p], 10) + "&";
+                                break;
+
+                            case 'number' :
+                            case 'string' :
+                                str += p + "=" + obj[p] + "&";
+                                break;
+
+                            case 'object' :
+                                for (var i in obj[p]) {
+                                    if (obj[p][i]) {
+                                        str += p + "[" + encodeURIComponent(i) + "]=" + encodeURIComponent(obj[p][i]) + "&";
+                                    }
+                                }
+                                break;
                         }
-                        break;
                     }
                 }
+
+                return str;
             }
-    
-            return str;
-        }
-        
-        var base = '';
-        var api = {};
-        
-        
-        
-        /**
-         * @api {public} Api.setBase('/api/v2/'); setBase()
-         * @apiName setBase
-         * @apiGroup API
-         *
-         * @apiParam {String} newBase The new base url for all $http requests.
-         *
-         * @apiSuccess {Null} null
-         */
-        api.setBase = function(newBase) {
-            base = newBase;
-        };
-        
-        
-        
-        /**
-         * @api {public} Api.get(url,data); get()
-         * @apiName get
-         * @apiGroup API
-         *
-         * @apiParam {String} url Get destination.
-         * @apiParam {Object} data Data to be sent with request.
-         *
-         * @apiSuccess {Promise} promise Standard $http promise.
-         */
-        api.get = function(url, data){
-            return $http.get(base + url + "?" + encodeObject(data));
-        };
-        
-        
-        
-        /**
-         * @api {public} Api.get(url,data); get()
-         * @apiName get
-         * @apiGroup API
-         *
-         * @apiParam {String} url Get destination.
-         * @apiParam {Object} data Data to be sent with request.
-         *
-         * @apiSuccess {Promise} promise Standard $http promise.
-         */
-        api.post = function(url, data){
-            return $http.post(base + url, data);
-        };
-        
-        
-        
-        /**
-         * @api {public} Api.get(url,data); get()
-         * @apiName get
-         * @apiGroup API
-         *
-         * @apiParam {String} url Get destination.
-         * @apiParam {Object} data Data to be sent with request.
-         *
-         * @apiSuccess {Promise} promise Standard $http promise.
-         */
-        api.put = function(url, data){
-            return $http.put(base + url, data);
-        };
-        
-        
-        
-        /**
-         * @api {public} Api.get(url,data); get()
-         * @apiName get
-         * @apiGroup API
-         *
-         * @apiParam {String} url Get destination.
-         * @apiParam {Object} data Data to be sent with request.
-         *
-         * @apiSuccess {Promise} promise Standard $http promise.
-         */
-        api.delete = function(url, data){
-            return $http.delete(base + url + "?" + encodeObject(data));
-        };
-        
-        return api;
-    }]);
-    
+
+            /**
+             * @api {public} Api.setBase('/api/v2/'); setBase()
+             * @apiName setBase
+             * @apiGroup API
+             *
+             * @apiParam {String} newBase The new base url for all $http requests.
+             *
+             * @apiSuccess {Null} null
+             */
+            this.setBase = function(newBase) {
+                this.base = newBase;
+            };
+
+            this.getBase = function() {
+                return this.base;
+            };
+
+            /**
+             * @api {public} Api.get(url,data); get()
+             * @apiName get
+             * @apiGroup API
+             *
+             * @apiParam {String} url Get destination.
+             * @apiParam {Object} data Data to be sent with request.
+             *
+             * @apiSuccess {Promise} promise Standard $http promise.
+             */
+            this.get = function(url, data) {
+                return $http.get(this.base + url + "?" + this.encodeObject(data));
+            };
+
+            /**
+             * @api {public} Api.get(url,data); get()
+             * @apiName get
+             * @apiGroup API
+             *
+             * @apiParam {String} url Get destination.
+             * @apiParam {Object} data Data to be sent with request.
+             *
+             * @apiSuccess {Promise} promise Standard $http promise.
+             */
+            this.post = function(url, data){
+                return $http.post(this.base + url, data);
+            };
+
+            /**
+             * @api {public} Api.get(url,data); get()
+             * @apiName get
+             * @apiGroup API
+             *
+             * @apiParam {String} url Get destination.
+             * @apiParam {Object} data Data to be sent with request.
+             *
+             * @apiSuccess {Promise} promise Standard $http promise.
+             */
+            this.put = function(url, data){
+                return $http.put(this.base + url, data);
+            };
+
+            /**
+             * @api {public} Api.get(url,data); get()
+             * @apiName get
+             * @apiGroup API
+             *
+             * @apiParam {String} url Get destination.
+             * @apiParam {Object} data Data to be sent with request.
+             *
+             * @apiSuccess {Promise} promise Standard $http promise.
+             */
+            this.delete = function(url, data){
+                return $http.delete(this.base + url + "?" + this.encodeObject(data));
+            };
+
+            return this;
+        }]);
+
 })();
---
+++ modules/api/test/apiSpec.js
@@ -0,0 +1,44 @@
+describe('angular.common: api', function() {
+    var scope, httpMock, api, http;
+
+    beforeEach(module('common.api'));
+
+    beforeEach(inject(function($rootScope, $controller, $httpBackend, Api, $http) {
+        scope = $rootScope.$new();
+        httpMock = $httpBackend;
+        api = Api;
+        http = $http;
+
+        httpMock.when('GET', '/api/v2/test').respond('resr');
+    }));
+
+    it('should set the base to an api', function() {
+        api.setBase('/api/v2/');
+        expect(api.getBase()).toBe('/api/v2/');
+    });
+
+    it('should convert and object to a query string', function() {
+        var data = {
+            userId: 1,
+            posts: ['test', 'test1']
+        };
+
+        expect(api.encodeObject(data), "userId=1&posts[0]=test&posts[1]=test1");
+    });
+
+    it('should make a GET request to a url', function() {
+        expect(api.get('/api/v2/')).toBeDefined();
+    });
+
+    it('should make a PUT request to a url', function() {
+        expect(api.put('/api/v2/')).toBeDefined();
+    });
+
+    it('should make a POST request to a url', function() {
+        expect(api.post('/api/v2/')).toBeDefined();
+    });
+
+    it('should make a  request to a url', function() {
+        expect(api.delete('/api/v2/')).toBeDefined();
+    });
+});
