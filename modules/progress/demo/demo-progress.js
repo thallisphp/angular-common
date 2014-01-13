be4eab4c3b08e14120973c4b40c11d257f564533
Michael Calkins <michael@clouddueling.com>
2014-01-12 16:01:27 -0800

    added a progress for showing the progress of an app

--- modules/progress/demo/demo-progress.js
+++ modules/progress/demo/demo-progress.js
@@ -6,11 +6,19 @@
         'common.progress'
     ])
 
-        .controller('DemoProgressCtrl', ['$scope', 'ProgressService', function($scope, ProgressService) {
+        .controller('DemoProgressCtrl', ['$scope', 'ProgressService', '$timeout', function($scope, ProgressService, $timeout) {
             $scope.ProgressService = ProgressService;
-            ProgressService.start('loadUser', 10);
-            ProgressService.start('loadTasks', 25);
-            ProgressService.done('loadUser');
+            $scope.startProgress = function() {
+                ProgressService.start('loadUser', 10);
+                $timeout(function() {
+                    ProgressService.done('loadUser');
+                }, 1000);
+
+                ProgressService.start('loadTasks', 25);
+                $timeout(function() {
+                    ProgressService.done('loadUser');
+                }, 2500);
+            };
 
             $scope.codeExample = "$scope.ProgressService = ProgressService;\n" +
                 "ProgressService.start('loadUser', 10);\n" +
