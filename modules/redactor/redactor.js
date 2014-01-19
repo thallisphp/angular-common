6721d961d6fc4d22a8bdbef9020c10d0ef868026
ThallisPHP <thallisphp@gmail.com>
2014-01-19 16:17:43 -0200

    Minha configuração

--- modules/redactor/redactor.js
+++ modules/redactor/redactor.js
@@ -15,10 +15,60 @@
 
                 // redactor
                 instance = element.redactor({
-                    imageGetJson: '/api/v1/redactor/images',
-                    imageUpload: '/api/v1/redactor/image?resource=true',
-                    fileUpload: '/api/v1/redactor/file',
-                    plugins: ['fontcolor', 'fullscreen'],
+                    lang: 'pt_br',
+                    buttons: [
+                    //  'html',
+                    //  '|',
+                        'formatting',
+                        '|',
+                        'bold',
+                        'italic',
+                        'deleted',
+                        '|',
+                        'unorderedlist',
+                        'orderedlist',
+                        'outdent',
+                        'indent',
+                        '|',
+                    //  'image',
+                        'video',
+                        'file',
+                        'table',
+                        'link',
+                        '|', 
+                        'alignment',
+                        '|',
+                        'horizontalrule'
+                    ],
+                //  imageGetJson: '/api/v1/redactor/images',
+                //  imageUpload: '/api/v1/redactor/image?resource=true',
+                //  fileUpload: '/api/v1/redactor/file',
+                    plugins: [
+                        'fontcolor',
+                        'fullscreen',
+                        'fontsize'
+                    ],
+                    deniedTags: [
+                        'html',
+                        'head',
+                        'link',
+                        'body',
+                        'meta',
+                        'script',
+                        'style',
+                        'applet',
+                        'video',
+                        'audio'
+                    ],
+                    mobile: false,
+                    convertImageLinks: true,
+                    convertVideoLinks: true,
+                    linkAnchor: false,
+                    linkNofollow: true,
+                    dragUpload: false,
+                    pastePlainText: true,
+                    tabSpaces: 0,
+                    linkSize: 20,
                     syncAfterCallback: function(html) {
                         // view -> model
                         if (initialised && controller.$viewValue !== html) {
